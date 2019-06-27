f5e93d27d5c0a7cffed22dc7cf52b9979c4de6d1
nimikm <nimikm@gmail.com>
2019-06-26 23:39:19 -0500

    excercise

---
+++ Chapters/Three/oneNine.py
@@ -0,0 +1,10 @@
+print('Enter')
+spam = input()
+if spam == '1':
+    print('Hello')
+elif spam == '2':
+    print('Howdy')
+else:
+    print('Greetings')
+
+        
---
+++ Chapters/Two/access.py
@@ -0,0 +1,9 @@
+name= 'Nimi'
+password = 'chinnu'
+
+if name == 'Nimi':
+    print ('Hello Nimi')
+    if password == 'chinnu':
+        print ('Access granted')
+    else:
+        print('Acess denied')
